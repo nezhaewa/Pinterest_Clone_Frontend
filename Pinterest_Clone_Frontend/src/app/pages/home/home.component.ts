71dad05704f7525412cdd6571ce9dda2e99a6c42
Mădă <78982140+u-mad-mary@users.noreply.github.com>
2025-07-27 05:03:16 +0300

    Update home.component.ts

--- Pinterest_Clone_Frontend/src/app/pages/home/home.component.ts
+++ Pinterest_Clone_Frontend/src/app/pages/home/home.component.ts
@@ -1,34 +1,3 @@
-// import { Component } from '@angular/core';
-// import { NgFor } from '@angular/common';
-
-// @Component({
-//   selector: 'app-home',
-//   standalone: true,
-//   imports: [NgFor],
-//   templateUrl: './home.component.html',
-//   styleUrls: ['./home.component.sass']
-// })
-// export class HomeComponent {
-//   pins = [
-//     {
-//       title: 'Stylish Coat',
-//       imageUrl: 'https://via.placeholder.com/300x400.png?text=Pin+1',
-//       altText: 'A stylish coat'
-//     },
-//     {
-//       title: 'Cute Cat',
-//       imageUrl: 'https://via.placeholder.com/300x300.png?text=Pin+2',
-//       altText: 'Cat image'
-//     },
-//     {
-//       title: 'Red Dress',
-//       imageUrl: 'https://via.placeholder.com/300x500.png?text=Pin+3',
-//       altText: 'Red dress'
-//     }
-   
-//   ];
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { NgFor } from '@angular/common';
 import { Api } from '../../services/api';
